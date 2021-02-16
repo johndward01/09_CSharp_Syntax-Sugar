51939cd83d1b3f78b4dd5329c16c2cf0ef4074f9
johndward01 <johndward01@gmail.com>
2021-02-16 15:46:23 -0600

    initial commit

---
+++ Syntax_Sugar_Demo/Syntax_Sugar_Demo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Syntax_Sugar_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Explicit Typing
+
+            string firstName = "John";
+            int age = 29;
+            bool isOver18 = true;
+
+            //Implicit Typing
+
+            var x = "abc";
+            var y = 2324;
+            var z = false;
+
+
+        }
+    }
+}
