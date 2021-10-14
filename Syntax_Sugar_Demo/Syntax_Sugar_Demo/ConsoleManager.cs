fd34fb7f20b5638930a6af9cf2bd89efbae909d0
johndward01 <johndward01@gmail.com>
2021-10-14 15:40:23 -0500

    update

---
+++ Syntax_Sugar_Demo/Syntax_Sugar_Demo/ConsoleManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Syntax_Sugar_Demo
+{
+
+    public class ConsoleManager
+    {
+        public int MyProperty { get; set; }
+
+        public void MyMethod()
+        {
+            Console.WriteLine("Something");
+        }
+
+    }
+}
--- Syntax_Sugar_Demo/Syntax_Sugar_Demo/Program.cs
+++ Syntax_Sugar_Demo/Syntax_Sugar_Demo/Program.cs
@@ -6,17 +6,50 @@ namespace Syntax_Sugar_Demo
     {
         static void Main(string[] args)
         {
-            //Explicit Typing
+            //// Explicit Typing
+            //string firstName = "John"; // Declaring & initializing
+            //int age = 29;
+            //bool isOver18 = true;
 
-            string firstName = "John";
-            int age = 29;
-            bool isOver18 = true;
+            //// Inferred Typing
+            //var x = "abc"; // Declaring & Initializing 
+            //var y = 2324;
+            //var z = false;
 
-            //Implicit Typing
+            //if (x == "abc" && !z)
+            //{
+            //    Console.WriteLine();
+            //}
+            //else
+            //{
+            //    Console.WriteLine();
+            //}
 
-            var x = "abc";
-            var y = 2324;
-            var z = false;
+
+            //var answer = (x == "abc" && !z) ? "Both of these are true" : "One of these was false";
+            //Console.WriteLine(answer);
+
+            int x = 0;
+            int y = 1;
+
+            //if (x > y)
+            //{
+            //    Console.WriteLine();
+            //}
+            //else
+            //{
+            //    Console.WriteLine();
+            //}
+
+            //var answer = (x > y) ? "x is greater" : "y is greater";
+            //Console.WriteLine(answer);
+
+            var answer1 = "0";
+            var answer2 = $"{0}";
+
+            Console.WriteLine(answer1);
+            Console.WriteLine(answer2);
+
 
 
         }
